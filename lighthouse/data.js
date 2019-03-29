834d7a7a7d349ccf4b5fafc2d1e8b9e3996730a3
Yuin Chien <yuin@google.com>
2019-03-29 11:20:14 -0700

    added rendertable

--- lighthouse/data.js
+++ lighthouse/data.js
@@ -25,14 +25,14 @@ const data_performance_opportunities = [
   },
   {
     color: 'red', title: 'Serve images in next-gen formats', time: '0.94',
-    description: 'Consider using <span class="code">&lt;link rel=preload></span> to prioritize fetching resources that are currently requested later in page load. Learn more.',
+    warning: 'Warnings: Unable to decode …201…/APV_HNNA_….jpg',
+    description: 'Image formats like JPEG 2000, JPEG XR, and WebP often provide better compression than PNG or JPEG, which means faster downloads and less data consumption. Learn more.',
     table: {
-      header: ['Opportunity', 'Estimated Savings'],
+      header: ['Image','','Size / Potential Savings'],
       items: [
-        ['…2.130.0/wp-video.css', '14.8 s'],
-        ['…2.130.0/wp-video.css', '14.8 s'],
-        ['…2.130.0/wp-video.css', '14.8 s'],
-        ['…2.130.0/wp-video.css', '14.8 s'],
+        ['https://cache-ssl.celtra.com/api/blobs/bef79596914041b1caba91fd1da0a8135f9ea4dd8a1488ce3edf9f034f732489/videoPoster.png', 'cache-ssl.celtra.com', '701 KB / 567 KB'],
+        ['https://cdn.cnn.com/cnnnext/dam/assets/190327231811-booker-13-large-tease.jpg', 'cdn.cnn.com', '32 KB / 23 KB'],
+        ['https://cdn.cnn.com/cnnnext/dam/assets/190322032309-0322-nz-reactions-11-large-tease.jpg', 'cdn.cnn.com', '33 KB / 17 KB'],
       ],
     },
   },
@@ -50,31 +50,58 @@ const data_performance_opportunities = [
       ],
     },
   },
-  // {
-  //   color: 'orange', title: 'Defer offscreen images', time: '4.31',
-  //   description: 'Consider lazy-loading offscreen and hidden images after all critical resources have finished loading to lower time to interactive. Learn more.',
-  //   table: {
-  //     header: ['Image','','Size', 'Potential Savings'],
-  //     items: [
-  //       ['https://cdn.cnn.com/cnnnext/dam/assets/151223171030-disneyland-file-large-tease.jpg', 'cdn.cnn.com', '59 KB', '59 KB'],
-  //       ['https://cdn.cnn.com/cnnnext/dam/assets/151223171030-disneyland-file-large-tease.jpg', 'cdn.cnn.com', '59 KB', '59 KB'],
-  //       ['https://cdn.cnn.com/cnnnext/dam/assets/151223171030-disneyland-file-large-tease.jpg', 'cdn.cnn.com', '59 KB', '59 KB'],
-  //     ],
-  //   },
-  // },
-  // {
-  //   color: 'green', title: 'Efficiently encode imagess', time: '0.16',
-  //   description: 'Consider using <span class="code">&lt;link rel=preload></span> to prioritize fetching resources that are currently requested later in page load. Learn more.',
-  //   table: {
-  //     header: ['Opportunity', 'Estimated Savings'],
-  //     items: [
-  //       ['…2.130.0/wp-video.css', '14.8 s'],
-  //       ['…2.130.0/wp-video.css', '14.8 s'],
-  //       ['…2.130.0/wp-video.css', '14.8 s'],
-  //       ['…2.130.0/wp-video.css', '14.8 s'],
-  //     ],
-  //   },
-  // },
+  {
+    color: 'orange', title: 'Defer offscreen images', time: '4.31',
+    description: 'Consider lazy-loading offscreen and hidden images after all critical resources have finished loading to lower time to interactive. Learn more.',
+    table: {
+      header: ['Image','','Size / Potential Savings'],
+      items: [
+        ['https://cdn.cnn.com/cnnnext/dam/assets/190329102439-hers-band-large-tease.jpg', 'cdn.cnn.com', '44 KB / 44 KB'],
+        ['https://cdn.cnn.com/cnnnext/dam/assets/190214103213-parkland-aniversario-large-tease.jpg', 'cdn.cnn.com', '43 KB / 43 KB'],
+        ['https://cdn.cnn.com/cnnnext/dam/assets/190328095939-raniya-wright-large-tease.jpg', 'cdn.cnn.com', '40 KB / 40 KB'],
+      ],
+    },
+  },
+  {
+    color: 'orange', title: 'Efficiently encode images', time: '0.18',
+    description: 'Optimized images load faster and consume less cellular data. Learn more.',
+    table: {
+      header: ['Image','','Size / Potential Savings'],
+      items: [
+        ['https://cdn.cnn.com/cnnnext/dam/assets/190327231811-booker-13-large-tease.jpg', 'cdn.cnn.com', '32 KB / 18 KB'],
+        ['https://cdn.cnn.com/cnnnext/dam/assets/190329102439-hers-band-small-tease.jpg', 'cdn.cnn.com', '23 KB / 12 KB'],
+        ['https://cdn.cnn.com/cnnnext/dam/assets/190305095620-02-paris-fashion-week-lead-image-medium-tease.jpg', 'cdn.cnn.com', '30 KB / 12 KB'],
+      ],
+    },
+  },
+  {
+    color: 'green', title: 'Preconnect to required origins', time: '0.45',
+    description: 'Consider adding preconnect or dns-prefetch resource hints to establish early connections to important third-party origins. Learn more.',
+    table: {
+      header: ['URL', 'Potential Savings'],
+      items: [
+        ['https://ads.yieldmo.com', '450 ms'],
+        ['https://gscounters.us1.gigya.com', '440 ms'],
+        ['https://bidder.criteo.com', '440 ms'],
+        ['https://ib.adnxs.com', '430 ms'],
+        ['https://api.rlcdn.com', '400 ms'],
+        ['https://ml314.com', '390 ms'],
+      ],
+    },
+  },
+  {
+    color: 'green', title: 'Remove unused CSS', time: '0.18',
+    description: 'Remove dead rules from stylesheets and defer the loading of CSS not used for above-the-fold content to reduce unnecessary bytes consumed by network activity. Learn more.',
+    table: {
+      header: ['URL', 'Size / Potential Savings'],
+      items: [
+        ['@charset "UTF-8";.zn.t-hidden{display:none!important} ...', '161 KB / 158 KB'],
+        ['…latest-2.x/css<span class="secondary">registry.api.cnn.io</span>', '54 KB / 51 KB'],
+        ['@charset "UTF-8";.zn.t-hidden{display:none!important} ...', '21 KB / 20 KB'],
+        ['…css/optanon.css<span class="secondary">cdn.cookielaw.org</span>', '6KB / KB'],
+      ],
+    },
+  },
 ];
 
 const data_performance_diagnostics = [
@@ -140,16 +167,16 @@ const data_performance_diagnostics = [
     color: 'red', title: 'Avoid an excessive DOM size <span class="red">&mdash;3,531 nodes</span>',
     description: 'Browser engineers recommend pages contain fewer than ~1,500 DOM nodes. The sweet spot is a tree depth < 32 elements and fewer than 60 children/parent element. A large DOM can increase memory usage, cause longer style calculations, and produce costly layout reflows. Learn more.',
     table: {
-      header: ['Statistic', 'Element', 'Value'],
+      header: ['Element', 'Statistic', 'Value'],
       items: [
-        ['Total DOM Nodes', '', '3,511'],
-        ['Maximum DOM Depth', '&lt;div aria-hidden="true" id="gigyaShareBar_0-reaction0-left_img">', '21'],
-        ['Maximum Child Elements', '&lt;head>', '155'],
+        ['', 'Total DOM Nodes', '3,511'],
+        ['&lt;div aria-hidden="true" id="gigyaShareBar_0-reaction0-left_img">', 'Maximum DOM Depth', '21'],
+        ['&lt;head>', 'Maximum Child Elements', '155'],
       ],
     },
   },
   {
-    color: 'gray', title: 'Avoid enormous network payloads <span class="red">&mdash;Total size was 5,029 KB</span>',
+    color: 'red', title: 'Avoid enormous network payloads <span class="red">&mdash;Total size was 5,029 KB</span>',
     description: 'Large network payloads cost users real money and are highly correlated with long load times. Learn more.',
     table: {
       header: ['URL', 'Size'],
@@ -175,5 +202,9 @@ const data_performance_diagnostics = [
         ['timeToZonesAndDomReady', 'Measure', '5,242 ms', '4,202.11 ms'],
       ],
     },
+  },
+  {
+    color: 'gray', title: 'Minimize Critical Requests Depth<span class="gray">&mdash;1 chain found</span>',
+    description: 'The Critical Request Chains below show you what resources are loaded with a high priority. Consider reducing the length of chains, reducing the download size of resources, or deferring the download of unnecessary resources to improve page load. Learn more.',
   },
 ];
